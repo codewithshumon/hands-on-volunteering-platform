f1e571657b0c21872a6ab6f04df3284ce968d8e8
Shumon <backlink.shumonkhan@gmail.com>
2025-05-25 00:56:07 +0600

    separate the api part

--- frontend/src/components/impact/CertificateBadge.jsx
+++
@@ -1,7 +0,0 @@
-const CertificateBadge = ({ certificate }) => (
-  <div className="bg-purple-50 px-3 py-1 rounded-full text-sm text-purple-700">
-    🏆 {certificate.title}
-  </div>
-);
-
-export default CertificateBadge;
